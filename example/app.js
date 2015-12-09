b70f0b0b4f6c863f5034e623c3836000190564d5
Alex Vitari <alexnoise79@gmail.com>
2015-12-09 22:12:26 +0100

    example live

---
+++ example/app.js
@@ -0,0 +1,99 @@
+/**
+ * Created by AlexNoise on 28/04/2015.
+ */
+
+'use strict';
+
+var app = angular.module('app',['ngLoadableContent','ngMockE2E']);//<<-- you have to import only ngLoadableContent
+
+/*  =========================================
+    JUST THIS EXAMPLE
+    ========================================= */
+//Fake XML HTTP REQUEST delayed require ngMockE2E !!!DO NOT IMPORT!!! in your projects as dependency
+app.run(function($httpBackend){
+    function round(i){return Math.floor(Math.random()*i);}
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    $httpBackend.whenGET('/email').respond(function(){return [200, false,{}]});
+
+    $httpBackend.whenPOST('/email').respond(function(method,url,data){return [200, re.test(data),{}]});
+
+    $httpBackend.whenGET('/items').respond(function(){return [200,[Math.random()*1,Math.random()*2, Math.random()*3],{}]});
+
+    $httpBackend.whenGET('/graph').respond(function(){return [200,[round(160),round(160),round(160),round(160),round(160),round(160),round(160),round(160),round(160),round(160)],{}]});
+});
+//Fake HTTP provider
+app.config(function($provide){$provide.decorator('$httpBackend',function($delegate){var p=function(m,u,d,c,h){var i=function(){var _this=this,_arguments=arguments;setTimeout(function(){c.apply(_this,_arguments);},(Math.random()*2e3)+1e3);};return $delegate.call(this,m,u,d,i,h);};for(var key in $delegate){p[key]=$delegate[key];}return p;});})
+/*  ========================================= */
+
+/*  =========================================
+     DEFAULT CONFIGURATION FOR YOUR PROJECT (override the default)
+    ========================================= */
+
+app.config(['$loaderConfigProvider',function ($loaderConfigProvider) {
+    $loaderConfigProvider.setDefault({color:"#0047ab"});
+}]);
+
+/*  =========================================
+     YOUR CONTROLLER EXAMPLE
+    ========================================= */
+
+app.controller('PageController', ['$loader', '$http', '$document', function ($loader, $http, $document) {
+    var page=this, toggle=true;
+
+    $document.bind('keypress', function (e) {
+        switch(e.which){
+            case 49:
+                page.req('graph', '/graph');
+                break;
+            case 50:
+                page.customStart('manual');
+                break;
+            case 51:
+                page.req('email','/email', page.name);
+                break;
+            case 52:
+                page.reloadImage('img1');
+                break;
+            case 32:
+                page.req('body');
+                break;
+        }
+    });
+
+    this.req = function (spinID, req, data) {
+        req = req || '/items';
+        $loader.spinElement(spinID, function(){  //<-- call the request inside
+            //call the fake http provider
+            if(data){
+                $http.post(req,data).success(function (data) {
+                    page.items = data;
+                    page[spinID] = data;
+                });
+            }else {
+                $http.get(req).success(function (data) {
+                    page.items = data;
+                    page[spinID] = data;
+                });
+            }
+        });
+    };
+
+    this.customStart = function(spinID){
+        if(toggle){
+            $loader.spinElement(spinID, function () {
+                $loader.startSpin();
+            });
+        }else{
+            $loader.stopSpin(spinID);
+        }
+        toggle= !toggle;
+    };
+
+    this.reloadImage = function (spinID) {
+        var $image = angular.element('[ng-loadable="'+spinID+'"]'),
+            oldSrc=$image.attr('src');
+        $loader.imageSpin($image);
+        $image.attr("src",oldSrc.substring(0,oldSrc.length-1)+(parseInt(oldSrc.substr(-1),10)+1));
+    }
+}])
---
+++ src/ngLoadableContent.js
@@ -0,0 +1,136 @@
+/**
+ * @author Alex Vitari.
+ * @revision 02-12-2015
+ * @update support img tag
+ */
+
+'use strict';
+
+angular.module('ngLoadableContent',[])
+    .config(['$httpProvider',function($httpProvider){
+        $httpProvider.interceptors.push('httpInterceptor');
+    }])
+    .provider('$loaderConfig', function () {
+        var defaults = {
+            overlay: true,
+            color: '#f60',
+            radius: 15,
+            width: 5,
+            length: 15,
+            lines: 13,
+            zIndex: 10,
+            speed: 1.5,
+            shadow: true,
+            top: '50%',
+            left: '50%'
+        };
+
+        return {
+            setDefault: function (conf) {
+                defaults = $.extend(defaults, conf);
+            },
+            $get: function () {
+                return  defaults;
+            }
+        };
+    })
+    .factory('httpInterceptor', ['$q', '$loader', function($q, $loader){
+        return {
+            'request': function(config){
+                config.headers.spinner=$loader.startSpin();//set the name of the spin
+                return config || $q.when(config);
+            },
+            'response': function(response){
+                $loader.stopSpin(response.config.headers.spinner);
+                return response || $q.when(response);
+            },
+            'requestError': function(rejection){
+                $loader.stopSpin(rejection.config.headers.spinner);
+                return $q.reject(rejection);
+            },
+            'responseError': function(rejection){
+                $loader.stopSpin(rejection.config.headers.spinner);
+                return $q.reject(rejection);
+            }
+        };
+    }])
+    .service("$loader", ['$rootScope','$loaderConfig', function($rootScope, $loaderConfig){
+        var currentSpinner='';
+
+        return {
+            "spinners" : [],
+            "spinElement": function(element, cb){
+                if(this.spinners.hasOwnProperty(element)){
+                    return false;
+                }else{
+                    $rootScope.$broadcast('loader.update', element);
+                    return cb();
+                }
+            },
+            "startSpin": function(){
+                var $container=this.spinners[currentSpinner].element;
+                if(this.spinners[currentSpinner].spinner.opts.overlay){
+                    $container.addClass('overlayed').prepend(angular.element('<div>',{'class':"overlay"}));
+                }
+                this.spinners[currentSpinner].spinner.spin($container[0]);
+                return currentSpinner;
+            },
+            "stopSpin": function(spinnerID){
+                if(this.spinners[spinnerID].spinner.opts.overlay){
+                    var element=this.spinners[spinnerID].element;
+
+                    element.removeClass('overlayed');
+                    if(element[0].tagName.toLowerCase()==='img'){
+                        element.prev('.overlay').remove();
+                    }else{
+                        element.children('.overlay').remove();
+                    }
+                }
+                this.spinners[spinnerID].spinner.stop();
+                delete this.spinners[spinnerID];
+            },
+            "setSpin": function($element, options){
+                currentSpinner = $element.attr("ng-loadable");
+                this.spinners[currentSpinner]={ element: $element, spinner: new window.Spinner($.extend(angular.copy($loaderConfig), options))};
+            },
+            "imageSpin" : function($element, options){
+                var loader = this,
+                    spinID = $element.attr("ng-loadable"),
+                    options = options || JSON.parse(JSON.stringify(eval('('+$element.attr("options")+')')));
+
+                if(this.spinners.hasOwnProperty(spinID)){
+                    return false;
+                }
+                this.setSpin($element, options);
+
+                $element.wrap('<span class="imageWrapper"></span>');
+                if(this.spinners[spinID].spinner.opts.overlay){
+                    $element.addClass('overlayed').before(angular.element('<div>',{'class':"overlay"}));
+                }
+                this.spinners[spinID].spinner.spin(this.spinners[spinID].element.parent()[0]);
+                $element[0].onload=function(){
+                    loader.stopSpin(spinID);
+                    $element.unwrap();
+                }
+            }
+        };
+    }])
+    .directive('ngLoadable', ['$loader', function($loader){
+        return {
+            restrict: 'A',
+            scope: {
+                options: '='
+            },
+            controller: function($scope, $element, $attrs){
+                $scope.$on('loader.update', function(event, name){
+                    if(name === $attrs.ngLoadable){
+                        $loader.setSpin($element, $scope.options);
+                    }
+                });
+
+                if($element[0].tagName.toLowerCase()==="img"){
+                    $loader.imageSpin($element,$scope.options);
+                }
+            }
+        };
+    }]);
